d2989ab672f90cf98c1cada7b065ddcf56f36886
Dustin Sallings <dustin@spy.net>
2005-08-26 18:50:10 +0000

    allow arbitrary modification of todo entries.

--- cgi/todo.py
+++ cgi/todo.py
@@ -28,13 +28,22 @@ What: <input type="text" name="w"/><br/>
 """ % {'rnd': random.Random().randint(0,10000)}
     return rv
 
-def getDoneForm():
+def getModifyForm():
     rv="""
-Completing $n ($i)<br/>
-<anchor title="Complete">
+Modifying $n ($i)<br/>
+<select name="a" title="Action">
+    <option value="check">Check</option>
+    <option value="delete">Delete</option>
+    <option value="mtop">Move to Top</option>
+    <option value="mup">Move Up</option>
+    <option value="mbottom">Move to Bottom</option>
+    <option value="mdown">Move Down</option>
+</select><br/>
+<anchor title="Modify">
     <go href="/cgi-bin/bp/todo.py?r=%(rnd)d" method="post">
         <postfield name="i" value="$(i)"/>
-        <postfield name="action" value="markDone"/>
+        <postfield name="a" value="$(a)"/>
+        <postfield name="action" value="modify"/>
     </go>
 </anchor>
 """ % {'rnd': random.Random().randint(0,10000)}
@@ -56,7 +65,7 @@ def doList(bp, fs):
 
     sendContent(wml(card("todo", "Todo list", out)
         + card("new", "New Todo", getNewForm())
-        + card("m", "Mark Done", getDoneForm())))
+        + card("m", "Modify Todo", getModifyForm())))
 
 def doAdd(bp, fs):
     what=fs["w"].value
@@ -65,13 +74,28 @@ def doAdd(bp, fs):
     sendContent(wml(card("added", "Added Todo",
         "Added a todo item with ID %d:  %s" % (id, text))))
 
-def markDone(bp, fs):
+def modify(bp, fs):
     id=int(fs["i"].value)
+    action=fs["a"].value
 
-    bp.list.toggle(getTodoId(), id)
-    sendContent(wml(card("toggled", "Completed",
-        "Your todo entry has been marked done.")))
+    actions={
+        "check": (bp.list.toggle,
+            "Toggled", "Your todo entry has been marked done."),
+        "delete": (bp.list.destroy,
+            "Deleted", "Your todo entry has been deleted."),
+        'mtop': (lambda a, b: bp.list.move(a, b, backpack.ListAPI.MOVE_TO_TOP),
+            "Moved", "Your todo entry has been moved to the top."),
+        'mup': (lambda a, b: bp.list.move(a, b, backpack.ListAPI.MOVE_HIGHER),
+            "Moved", "Your todo entry has been moved up."),
+        'mbottom': (lambda a, b:
+                bp.list.move(a, b, backpack.ListAPI.MOVE_TO_BOTTOM),
+            "Moved", "Your todo entry has moved to the bottom."),
+        'mdown': (lambda a, b: bp.list.move(a, b, backpack.ListAPI.MOVE_LOWER),
+            "Moved", "Your todo entry has moved lower."),
+    }
 
+    actions[action][0](getTodoId(), id)
+    sendContent(wml(card("modified", actions[action][1], actions[action][2])))
 
 if __name__ == '__main__':
-    doCallback({"list": doList, "add": doAdd, "markDone": markDone})
+    doCallback({"list": doList, "add": doAdd, "modify": modify})
